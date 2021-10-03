d0f103def95e991487311d8ed4d8794be4d5d210
egriffit <egrif017@odu.edu>
2021-10-03 18:19:53 -0400

    Add framework_day entity and test case

---
+++ app/src/main/java/com/example/workout_companion/entity/FrameworkDayEntity.kt
@@ -0,0 +1,32 @@
+package com.example.workout_companion.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+/**
+ * An entity representing the framework_day table in the database
+ *
+ * @property id                 the primary key id of each entry in the table.
+ * @property framework_type_id  the foreign key pointing to the framework_type that contains this day.
+ * @property name               the name of the day
+ */
+@Entity(tableName = "framework_day",
+        foreignKeys = [ForeignKey(entity = FrameworkTypeEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["framework_type_id"],
+            onDelete = ForeignKey.CASCADE)
+        ]
+)
+data class FrameworkDayEntity(
+    @PrimaryKey(autoGenerate = false)
+    @ColumnInfo(name = "id")
+    var id: Int,
+
+    @ColumnInfo(name = "framework_type_id")
+    var framework_type_id: Int,
+
+    @ColumnInfo(name = "name")
+    var name: String,
+)
