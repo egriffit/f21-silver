8b9be9db68f46aa4cd3651a191ea171687b2385c
pwhitfie <pwhit011@odu.edu>
2021-10-11 12:15:37 -0400

    add goals view in progress

--- app/src/main/java/com/example/workout_companion/view/AddGoalView.kt
+++ app/src/main/java/com/example/workout_companion/view/AddGoalView.kt
@@ -3,11 +3,21 @@ package com.example.workout_companion.view
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 
 @Composable
 fun AddGoalView(navController: NavController){
     Row(){
-        Text("Add Goal View")
+        Text("Add Goals View")
     }
+
+    Row(){
+        Text("Select Nutrition Plan")
+    }
+
+    Row(){
+        Text("Select Workout Plan")
+    }
+
 }
