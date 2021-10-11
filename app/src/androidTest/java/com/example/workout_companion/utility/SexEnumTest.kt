e577c548c7209dde0a07a64880fadc6b2309af41
egriffit <egrif017@odu.edu>
2021-10-11 16:26:06 -0400

    Add sex enumeration

---
+++ app/src/androidTest/java/com/example/workout_companion/utility/SexEnumTest.kt
@@ -0,0 +1,30 @@
+package com.example.workout_companion.utility
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import junit.framework.TestCase
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.lang.IllegalArgumentException
+
+@RunWith(AndroidJUnit4::class)
+class SexEnumTest {
+    
+    @Test
+    fun convertToString() {
+        val enum = Sex.MALE
+        TestCase.assertEquals(SexConverter.fromSex(enum), "Male")
+    }
+
+    @Test
+    fun convertToSex() {
+        val name = "Female"
+        TestCase.assertEquals(SexConverter.toSex(name), Sex.FEMALE)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun badConvertToSex() {
+        val badString = "BAD"
+        SexConverter.toSex(badString)
+    }
+
+}
---
+++ app/src/main/java/com/example/workout_companion/utility/SexEnum.kt
@@ -0,0 +1,54 @@
+package com.example.workout_companion.utility
+
+import androidx.room.TypeConverter
+import java.lang.IllegalArgumentException
+
+/**
+ * Enumeration for biological sexes
+ *
+ * @property descName The descriptive name of the biological sex.
+ */
+enum class Sex(val descName: String) {
+    MALE("Male"),
+    FEMALE("Female"),
+}
+
+/**
+ * Converter between biological sexes and their descriptive names
+ */
+object SexConverter {
+
+    /**
+     * Convert a sex to its descriptive name
+     *
+     * @property sex The enumerated sex to convert.
+     *
+     * @return The descriptive name.
+     */
+    @TypeConverter
+    @JvmStatic
+    fun fromSex(sex: Sex): String {
+        return sex.descName
+    }
+
+    /**
+     * Convert a descriptive name to its sex enumeration
+     *
+     * @property name The descriptive name of the enumeration.
+     *
+     * @return The enumerated sex.
+     *
+     * @throws IllegalArgumentException
+     */
+    @TypeConverter
+    @JvmStatic
+    fun toSex(name: String): Sex {
+        for (enum in Sex.values()) {
+            if (enum.descName == name) {
+                return enum
+            }
+        }
+
+        throw IllegalArgumentException("$name is not a valid biological sex")
+    }
+}
