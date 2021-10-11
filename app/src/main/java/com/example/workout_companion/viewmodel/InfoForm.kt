591efc72d1d91e6b0b42284f14bc90f008310993
dsimmons <dsimm008@odu.edu>
2021-10-11 19:53:41 -0400

    Info Form Commit

---
+++ app/src/main/java/com/example/workout_companion/viewmodel/InfoForm.kt
@@ -0,0 +1,138 @@
+package com.example.workout_companion.viewmodel
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.workout_companion.view.ui.theme.Workout_companionTheme
+
+class InfoForm : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Workout_companionTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    LazyColumnDemo();
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview3() {
+    Workout_companionTheme {
+         LazyColumnDemo();
+    }
+}
+
+@Composable
+fun LazyColumnDemo(){
+    Column(
+        Modifier
+            .background(Color(0xFFEDEAE0))
+            .fillMaxSize()
+            .padding(32.dp),
+        verticalArrangement = Arrangement.spacedBy(24.dp)
+    ) {
+        var textState by remember { mutableStateOf(TextFieldValue()) }
+
+        var query = remember { mutableStateOf(" ")}
+
+        OutlinedTextField(
+            value = query.value,
+            onValueChange = { query.value = it },
+            label = { Text(text = "Name?") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text
+            )
+        )
+
+        OutlinedTextField(
+            value = query.value,
+            onValueChange = { query.value = it },
+            label = { Text(text = "Height?") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text
+            )
+        )
+
+        OutlinedTextField(
+            value = textState,
+            onValueChange = { textState = it },
+            label = { Text(text = "Activity Level?") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text
+            )
+        )
+
+        OutlinedTextField(
+            value = textState,
+            onValueChange = { textState = it },
+            label = { Text(text = "Age?") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            )
+        )
+
+        OutlinedTextField(
+            value = textState,
+            onValueChange = { textState = it },
+            label = { Text(text = "Gender?") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text
+            )
+
+        )
+
+        OutlinedTextField(
+            value = textState,
+            onValueChange = { textState = it },
+            label = { Text(text = "Main Goal?") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text
+            )
+        )
+
+        OutlinedTextField(
+            value = textState,
+            onValueChange = { textState = it },
+            label = { Text(text = "Experience? (Beginner, expert)") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text
+            )
+        )
+        OutlinedTextField(
+            value = textState,
+            onValueChange = { textState = it },
+            label = { Text(text = "Weight? (In pounds)") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            )
+        )
+    }
+}
+
