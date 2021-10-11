0658212180f3a9b19a8859372ba009711e39f9b0
pwhitfie <pwhit011@odu.edu>
2021-10-11 12:21:30 -0400

    before merging in from main

--- app/src/main/java/com/example/workout_companion/view/AddGoalView.kt
+++ app/src/main/java/com/example/workout_companion/view/AddGoalView.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.workout_companion.viewmodel.MacronutrientSelector
 
 @Composable
 fun AddGoalView(navController: NavController){
@@ -12,12 +14,6 @@ fun AddGoalView(navController: NavController){
         Text("Add Goals View")
     }
 
-    Row(){
-        Text("Select Nutrition Plan")
-    }
-
-    Row(){
-        Text("Select Workout Plan")
-    }
+    MacronutrientSelector(canSubmit = true)
 
 }
