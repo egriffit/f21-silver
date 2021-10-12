cfa83e2a1dc0640e0ca0ebb06f47b8a477f71063
dsimmons <dsimm008@odu.edu>
2021-10-11 21:12:36 -0400

    Info Form Commit

--- app/src/main/java/com/example/workout_companion/viewmodel/InfoForm.kt
+++ app/src/main/java/com/example/workout_companion/viewmodel/InfoForm.kt
@@ -3,19 +3,20 @@ package com.example.workout_companion.viewmodel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.workout_companion.view.ui.theme.Workout_companionTheme
 
@@ -26,7 +27,7 @@ class InfoForm : ComponentActivity() {
             Workout_companionTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    LazyColumnDemo();
+                    LazyColumnDemo(); submitbutton();
                 }
             }
         }
@@ -37,7 +38,7 @@ class InfoForm : ComponentActivity() {
 @Composable
 fun DefaultPreview3() {
     Workout_companionTheme {
-         LazyColumnDemo();
+         LazyColumnDemo(); submitbutton()
     }
 }
 
@@ -48,15 +49,21 @@ fun LazyColumnDemo(){
             .background(Color(0xFFEDEAE0))
             .fillMaxSize()
             .padding(32.dp),
-        verticalArrangement = Arrangement.spacedBy(24.dp)
+        verticalArrangement = Arrangement.spacedBy(5.dp)
     ) {
-        var textState by remember { mutableStateOf(TextFieldValue()) }
-
-        var query = remember { mutableStateOf(" ")}
+        var name by remember { mutableStateOf(TextFieldValue()) }
+        var heightfeet by remember { mutableStateOf(TextFieldValue())}
+        var heightinch by remember { mutableStateOf(TextFieldValue())}
+        var activitylevel by remember { mutableStateOf(TextFieldValue())}
+        var age by remember { mutableStateOf(TextFieldValue())}
+        var gender by remember { mutableStateOf(TextFieldValue())}
+        var maingoal by remember { mutableStateOf(TextFieldValue())}
+        var experience by remember { mutableStateOf(TextFieldValue())}
+        var weight by remember { mutableStateOf(TextFieldValue())}
 
         OutlinedTextField(
-            value = query.value,
-            onValueChange = { query.value = it },
+            value = name,
+            onValueChange = { name = it },
             label = { Text(text = "Name?") },
             modifier = Modifier.fillMaxWidth(),
             keyboardOptions = KeyboardOptions(
@@ -65,18 +72,28 @@ fun LazyColumnDemo(){
         )
 
         OutlinedTextField(
-            value = query.value,
-            onValueChange = { query.value = it },
-            label = { Text(text = "Height?") },
+            value = heightfeet,
+            onValueChange = { heightfeet = it },
+            label = { Text(text = "Height? (in feet)") },
             modifier = Modifier.fillMaxWidth(),
             keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Text
+                keyboardType = KeyboardType.Number
             )
         )
 
         OutlinedTextField(
-            value = textState,
-            onValueChange = { textState = it },
+            value = heightinch,
+            onValueChange = { heightinch = it },
+            label = { Text(text = "Height? (in inches") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            )
+        )
+
+        OutlinedTextField(
+            value = activitylevel,
+            onValueChange = { activitylevel = it },
             label = { Text(text = "Activity Level?") },
             modifier = Modifier.fillMaxWidth(),
             keyboardOptions = KeyboardOptions(
@@ -85,8 +102,8 @@ fun LazyColumnDemo(){
         )
 
         OutlinedTextField(
-            value = textState,
-            onValueChange = { textState = it },
+            value = age,
+            onValueChange = { age = it },
             label = { Text(text = "Age?") },
             modifier = Modifier.fillMaxWidth(),
             keyboardOptions = KeyboardOptions(
@@ -95,8 +112,8 @@ fun LazyColumnDemo(){
         )
 
         OutlinedTextField(
-            value = textState,
-            onValueChange = { textState = it },
+            value = gender,
+            onValueChange = { gender = it },
             label = { Text(text = "Gender?") },
             modifier = Modifier.fillMaxWidth(),
             keyboardOptions = KeyboardOptions(
@@ -106,8 +123,8 @@ fun LazyColumnDemo(){
         )
 
         OutlinedTextField(
-            value = textState,
-            onValueChange = { textState = it },
+            value = maingoal,
+            onValueChange = { maingoal = it },
             label = { Text(text = "Main Goal?") },
             modifier = Modifier.fillMaxWidth(),
             keyboardOptions = KeyboardOptions(
@@ -116,8 +133,8 @@ fun LazyColumnDemo(){
         )
 
         OutlinedTextField(
-            value = textState,
-            onValueChange = { textState = it },
+            value = experience,
+            onValueChange = { experience = it },
             label = { Text(text = "Experience? (Beginner, expert)") },
             modifier = Modifier.fillMaxWidth(),
             keyboardOptions = KeyboardOptions(
@@ -125,8 +142,8 @@ fun LazyColumnDemo(){
             )
         )
         OutlinedTextField(
-            value = textState,
-            onValueChange = { textState = it },
+            value = weight,
+            onValueChange = { weight = it },
             label = { Text(text = "Weight? (In pounds)") },
             modifier = Modifier.fillMaxWidth(),
             keyboardOptions = KeyboardOptions(
@@ -136,3 +153,28 @@ fun LazyColumnDemo(){
     }
 }
 
+@Composable
+fun submitbutton(){
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+
+// Modifies the button to be aligned properly
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    )
+
+    { Button(
+        onClick = {},
+        modifier = Modifier.padding(all = Dp(18F)),
+        enabled = true,
+        border = BorderStroke(width = 1.dp, brush = SolidColor(Color.Magenta)),
+        shape = MaterialTheme.shapes.large,
+    )
+    {
+        Text(text = "Submit?", color = Color.White)
+    }
+    }
+}
