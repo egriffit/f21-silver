d130c1071543b23f98527ebf267aa89b5b5abc4d
egriffit <egrif017@odu.edu>
2021-10-21 20:07:56 -0400

    Update descriptive name in maingoal to val

--- app/src/main/java/com/example/workout_companion/utility/MainGoalEnum.kt
+++ app/src/main/java/com/example/workout_companion/utility/MainGoalEnum.kt
@@ -6,9 +6,9 @@ import java.lang.IllegalArgumentException
 /**
  * Enumeration of the possible goals the user can select
  *
- * @param descName The descriptive name of the enumeration.
+ * @property descName The descriptive name of the enumeration.
  */
-enum class MainGoal(descName: String) {
+enum class MainGoal(val descName: String) {
     BUILD_MUSCLE("Build Muscle"),
     INCREASE_STRENGTH("Increase Strength"),
     LOSE_WEIGHT("Lose Weight"),
