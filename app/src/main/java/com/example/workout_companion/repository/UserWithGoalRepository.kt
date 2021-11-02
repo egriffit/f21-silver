27e0e620ac9983a853e2799aec3a6c28c5ab415e
egriffit <egrif017@odu.edu>
2021-11-01 20:33:55 -0400

    Document and create repo test

--- app/src/main/java/com/example/workout_companion/repository/UserWithGoalRepository.kt
+++ app/src/main/java/com/example/workout_companion/repository/UserWithGoalRepository.kt
@@ -2,6 +2,14 @@ package com.example.workout_companion.repository
 
 import com.example.workout_companion.dao.UserWithGoalDao
 
+/**
+ * Repository for accessing a user and their attached goal
+ *
+ * @property dao The Data Access Object for the underlying data
+ */
 class UserWithGoalRepository(private val dao: UserWithGoalDao) {
+    /**
+     * The user with goal value
+     */
     val userWithGoal = dao.getUserWithGoal()
 }
--- app/src/main/java/com/example/workout_companion/viewmodel/UserWithGoalViewModel.kt
+++ app/src/main/java/com/example/workout_companion/viewmodel/UserWithGoalViewModel.kt
@@ -5,18 +5,23 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import com.example.workout_companion.dao.UserWithGoal
 import com.example.workout_companion.database.WCDatabase
-import com.example.workout_companion.repository.UserRepository
 import com.example.workout_companion.repository.UserWithGoalRepository
 
+/**
+ * ViewModel for getting a user and their attached goal
+ *
+ * @property application The current application
+ */
 class UserWithGoalViewModel(application: Application) : AndroidViewModel(application) {
 
+    /**
+     * The value of the user and their goal
+     */
     val userWithGoal: LiveData<UserWithGoal>
-
-    private val repository: UserWithGoalRepository
 
     init {
         val dao = WCDatabase.getInstance(application).userWithGoalDao()
-        repository = UserWithGoalRepository(dao)
+        val repository = UserWithGoalRepository(dao)
         userWithGoal = repository.userWithGoal
     }
 }
