6b424283a36bf3026ce6ca4711f4f8bb2afa4285
egriffit <egrif017@odu.edu>
2021-11-01 20:50:27 -0400

    Remove unnecessary changes to goaltypedao

--- app/src/main/java/com/example/workout_companion/dao/GoalTypeDao.kt
+++ app/src/main/java/com/example/workout_companion/dao/GoalTypeDao.kt
@@ -16,11 +16,11 @@ import com.example.workout_companion.entity.GoalTypeEntity
 interface GoalTypeDao {
 
     /**
-     * Gets all goals in the GoalTypeEntity table sorted by primary key
+     * Gets all goals in the GoalTypeEntity table
      *
      * @return a LiveData variable containing all GoalTypeEntity values in the table.
      */
-    @Query("SELECT * FROM goal_type ORDER BY id ASC")
+    @Query("SELECT * FROM goal_type")
     fun getAllGoals(): LiveData<List<GoalTypeEntity>>
 
     /**
