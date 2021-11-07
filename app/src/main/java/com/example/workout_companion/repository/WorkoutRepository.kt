4701dbdfdf4d0d3396797d530c02fde5e8a846b4
egriffit <egrif017@odu.edu>
2021-11-07 17:27:27 -0500

    Document workout repository and view model

--- app/src/main/java/com/example/workout_companion/repository/WorkoutRepository.kt
+++ app/src/main/java/com/example/workout_companion/repository/WorkoutRepository.kt
@@ -5,22 +5,52 @@ import com.example.workout_companion.dao.WorkoutDao
 import com.example.workout_companion.entity.WorkoutEntity
 import java.time.LocalDate
 
+/**
+ * Repository for access to workouts
+ *
+ * @property workoutDao Dao for accessing the workout tables in the database
+ */
 class WorkoutRepository(private val workoutDao: WorkoutDao) {
 
+    /**
+     * All workouts in the database
+     */
     val workouts = workoutDao.getAllWorkouts()
 
+    /**
+     * Gets the workout on a specific date
+     *
+     * @property date The date for the workout
+     *
+     * @return The workout
+     */
     fun getWorkoutOnDate(date: LocalDate): LiveData<WorkoutEntity> {
         return workoutDao.getWorkoutOnDate(date)
     }
 
+    /**
+     * Adds a workout to the database
+     *
+     * @property workout The workout to add
+     */
     suspend fun addWorkout(workout: WorkoutEntity) {
         workoutDao.addWorkout(workout)
     }
 
+    /**
+     * Updates a workout in the database
+     *
+     * @property workout The workout to update
+     */
     suspend fun updateWorkout(workout: WorkoutEntity) {
         workoutDao.updateWorkout(workout)
     }
 
+    /**
+     * Deletes a workout from the database
+     *
+     * @property workout The workout to delete
+     */
     suspend fun deleteWorkout(workout: WorkoutEntity) {
         workoutDao.deleteWorkout(workout)
     }
--- app/src/main/java/com/example/workout_companion/viewmodel/WorkoutViewModel.kt
+++ app/src/main/java/com/example/workout_companion/viewmodel/WorkoutViewModel.kt
@@ -8,10 +8,21 @@ import com.example.workout_companion.entity.WorkoutEntity
 import com.example.workout_companion.repository.WorkoutRepository
 import java.time.LocalDate
 
+/**
+ * ViewModel for the workout table in the database
+ *
+ * @property application The application
+ */
 class WorkoutViewModel(application: Application) : AndroidViewModel(application) {
 
+    /**
+     * The workouts in the database
+     */
     val workouts: LiveData<List<WorkoutEntity>>
 
+    /**
+     * The repository for database access
+     */
     private val repository: WorkoutRepository
 
     init {
@@ -20,18 +31,40 @@ class WorkoutViewModel(application: Application) : AndroidViewModel(application)
         workouts = repository.workouts
     }
 
+    /**
+     * Get a workout from the database on a specific date
+     *
+     * @property date The date of the workout
+     *
+     * @return The workout from the specific date
+     */
     fun getWorkoutOnDate(date: LocalDate): LiveData<WorkoutEntity> {
         return repository.getWorkoutOnDate(date)
     }
 
+    /**
+     * Add a workout to the database
+     *
+     * @property workout The workout to add
+     */
     suspend fun addWorkout(workout: WorkoutEntity) {
         repository.addWorkout(workout)
     }
 
+    /**
+     * Update a workout in the database
+     *
+     * @property workout The workout to update
+     */
     suspend fun updateWorkout(workout: WorkoutEntity) {
         repository.updateWorkout(workout)
     }
 
+    /**
+     * Delete a workout from the database
+     *
+     * @property workout The workout to delete
+     */
     suspend fun deleteWorkout(workout: WorkoutEntity) {
         repository.deleteWorkout(workout)
     }
