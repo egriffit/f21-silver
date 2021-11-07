4caed999c0dcdf8e149a0f6cd90900429f2058a7
egriffit <egrif017@odu.edu>
2021-11-07 15:27:34 -0500

    Create workout dao

---
+++ app/src/main/java/com/example/workout_companion/dao/WorkoutDao.kt
@@ -0,0 +1,25 @@
+package com.example.workout_companion.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.workout_companion.entity.WorkoutEntity
+import java.time.LocalDate
+
+@Dao
+interface WorkoutDao {
+
+    @Query("SELECT * from workout")
+    fun getAllWorkouts(): LiveData<List<WorkoutEntity>>
+
+    @Query("SELECT * FROM workout WHERE date=:date")
+    fun getWorkoutOnDate(date: LocalDate)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun addWorkout(workout: WorkoutEntity)
+
+    @Update
+    fun updateWorkout(workout: WorkoutEntity)
+
+    @Delete
+    fun deleteWorkout(workout: WorkoutEntity)
+}
