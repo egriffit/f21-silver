4bcac55b65512931c504434b74929740639cd729
egriffit <egrif017@odu.edu>
2021-11-07 14:46:38 -0500

    Create workout entity

---
+++ app/src/main/java/com/example/workout_companion/entity/WorkoutEntity.kt
@@ -0,0 +1,34 @@
+package com.example.workout_companion.entity
+
+import androidx.room.*
+import com.example.workout_companion.enumeration.Progress
+import com.example.workout_companion.enumeration.ProgressConverter
+import com.example.workout_companion.utility.DateTimeConverter
+import java.time.LocalDate
+
+@Entity(tableName = "workout",
+        foreignKeys = [ForeignKey(entity = FrameworkTypeEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["framework_id"],
+            onDelete = ForeignKey.NO_ACTION
+        )]
+)
+data class WorkoutEntity(
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    var id: Int,
+
+    @ColumnInfo(name = "date")
+    @TypeConverters(DateTimeConverter::class)
+    var date: LocalDate,
+
+    @ColumnInfo(name = "status")
+    @TypeConverters(ProgressConverter::class)
+    var status: Progress,
+
+    @ColumnInfo(name = "name")
+    var name: String,
+
+    @ColumnInfo(name = "framework_id", index = true)
+    var framework_id: Int,
+)
