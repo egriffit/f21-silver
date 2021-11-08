226e4e5ab8303e41746970ff1f8d50ef65cd4d69
egriffit <egrif017@odu.edu>
2021-11-08 17:37:15 -0500

    Document all classes

--- app/src/main/java/com/example/workout_companion/viewmodel/FrameworkComponentSetViewModel.kt
+++ app/src/main/java/com/example/workout_companion/viewmodel/FrameworkComponentSetViewModel.kt
@@ -7,8 +7,16 @@ import com.example.workout_companion.entity.FrameworkComponentSetEntity
 import com.example.workout_companion.repository.FrameworkComponentSetRepository
 import java.time.LocalDate
 
+/**
+ * View model for the framework_component_set table in the database
+ *
+ * @param application The application.
+ */
 class FrameworkComponentSetViewModel(application: Application) : AndroidViewModel(application) {
 
+    /**
+     * The repository for accessing the database
+     */
     private val repository: FrameworkComponentSetRepository
 
     init {
@@ -16,18 +24,40 @@ class FrameworkComponentSetViewModel(application: Application) : AndroidViewMode
         repository = FrameworkComponentSetRepository(dao)
     }
 
+    /**
+     * Gets all framework component sets for the given date
+     *
+     * @param date The date to search with.
+     *
+     * @return A list of all framework component sets.
+     */
     suspend fun getFrameworkComponentSetsForDate(date: LocalDate): List<FrameworkComponentSetEntity> {
         return repository.getFrameworkComponentSetsForDate(date)
     }
 
+    /**
+     * Adds a framework component set to the database
+     *
+     * @param set The set to add to the database.
+     */
     suspend fun addFrameworkComponentSet(set: FrameworkComponentSetEntity) {
         repository.addFrameworkComponentSet(set)
     }
 
+    /**
+     * Updates a framework component set in the database.
+     *
+     * @param set The set to update in the database.
+     */
     suspend fun updateFrameworkComponentSet(set: FrameworkComponentSetEntity) {
         repository.updateFrameworkComponentSet(set)
     }
 
+    /**
+     * Delete a framework component set from the database.
+     *
+     * @param set The set to remove from the database.
+     */
     suspend fun deleteFrameworkComponentSet(set: FrameworkComponentSetEntity) {
         repository.deleteFrameworkComponentSet(set)
     }
