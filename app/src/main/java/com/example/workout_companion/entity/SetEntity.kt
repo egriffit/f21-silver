57c1970004c54e0e13bbbebaeabd4fac8890401c
egriffit <egrif017@odu.edu>
2021-11-22 14:45:04 -0500

    Create set entity class

---
+++ app/src/main/java/com/example/workout_companion/entity/SetEntity.kt
@@ -0,0 +1,34 @@
+package com.example.workout_companion.entity
+
+import androidx.room.*
+import com.example.workout_companion.enumeration.Progress
+import com.example.workout_companion.enumeration.ProgressConverter
+
+@Entity(tableName = "set",
+        foreignKeys = [
+            ForeignKey(
+                entity = FrameworkComponentSetEntity::class,
+                parentColumns = ["id"],
+                childColumns = ["framework_component_set_id"],
+                onDelete = ForeignKey.CASCADE
+            )
+        ]
+)
+data class SetEntity(
+    @PrimaryKey(autoGenerate = false)
+    @ColumnInfo(name = "id")
+    var id: Int,
+
+    @ColumnInfo(name = "framework_component_set_id", index = true)
+    var framework_component_set_id: Int,
+
+    @ColumnInfo(name = "reps")
+    var reps: Int,
+
+    @ColumnInfo(name = "weight")
+    var weight: Double,
+
+    @TypeConverters(ProgressConverter::class)
+    @ColumnInfo(name = "status")
+    var status: Progress
+)
