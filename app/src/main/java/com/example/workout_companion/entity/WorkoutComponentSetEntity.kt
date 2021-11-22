6fb6ddb24e880103d9b103aaadc6a4e28e933fc6
egriffit <egrif017@odu.edu>
2021-11-22 16:14:02 -0500

    Rename set entity to workout component set

--- app/src/main/java/com/example/workout_companion/entity/WorkoutComponentSetEntity.kt
+++ app/src/main/java/com/example/workout_companion/entity/WorkoutComponentSetEntity.kt
@@ -8,29 +8,29 @@ import com.example.workout_companion.enumeration.ProgressConverter
  * Entity representing the sets performed during a workout stored in the database
  *
  * @param id The primary key id
- * @param framework_component_set_id The foreign key id of the framework component this set comes from
+ * @param workout_component_id The foreign key id of the framework component this set comes from
  * @param reps The repetitions performed during this set
  * @param weight The weight used during this set
  * @param status The state of the set
  * @param wger_id The foreign key id of the wger exercise
  */
-@Entity(tableName = "set",
+@Entity(tableName = "workout_component_set",
         foreignKeys = [
             ForeignKey(
                 entity = WorkoutComponentEntity::class,
                 parentColumns = ["id"],
-                childColumns = ["framework_component_set_id"],
+                childColumns = ["workout_component_id"],
                 onDelete = ForeignKey.CASCADE
             )
         ]
 )
-data class SetEntity(
+data class WorkoutComponentSetEntity(
     @PrimaryKey(autoGenerate = false)
     @ColumnInfo(name = "id")
     var id: Int,
 
-    @ColumnInfo(name = "framework_component_set_id", index = true)
-    var framework_component_set_id: Int,
+    @ColumnInfo(name = "workout_component_id", index = true)
+    var workout_component_id: Int,
 
     @ColumnInfo(name = "reps")
     var reps: Int,
--- app/src/test/java/com/example/workout_companion/entity/WorkoutComponentSetEntityTest.kt
+++ app/src/test/java/com/example/workout_companion/entity/WorkoutComponentSetEntityTest.kt
@@ -4,20 +4,20 @@ import com.example.workout_companion.enumeration.Progress
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
-class SetEntityTest {
+class WorkoutComponentSetEntityTest {
 
     @Test
     fun constructorTest() {
         val id = 0
-        val frameworkComponentSetId = 0
+        val workoutComponentId = 0
         val reps = 30
         val weight = 185.0
         val progress = Progress.IN_PROGRESS
         val wgerId = 5
-        val set = SetEntity(id, frameworkComponentSetId, reps, weight, progress, wgerId)
+        val set = WorkoutComponentSetEntity(id, workoutComponentId, reps, weight, progress, wgerId)
 
         assertEquals(id, set.id)
-        assertEquals(frameworkComponentSetId, set.framework_component_set_id)
+        assertEquals(workoutComponentId, set.workout_component_id)
         assertEquals(reps, set.reps)
         assertEquals(weight, set.weight, 0.01)
         assertEquals(progress, set.status)
