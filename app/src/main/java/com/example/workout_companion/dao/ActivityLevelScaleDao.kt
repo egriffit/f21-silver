0fa85936a7ea935d3f00ef8e46d5e0921ac09a89
fudgenuggets1 <fudgenuggets1@live.com>
2021-12-09 00:57:56 -0500

    Create activity_level_scale entity, dao, and repository

---
+++ app/src/main/java/com/example/workout_companion/dao/ActivityLevelScaleDao.kt
@@ -0,0 +1,23 @@
+package com.example.workout_companion.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.workout_companion.entity.ActivityLevelScaleEntity
+
+
+/**
+ *
+ * The Data Abstraction Object for the ActivityLevelScaleEntity
+ *
+ * Provides methods for SQL queries using the ActivityLevelScaleEntity
+ *
+ */
+
+@Dao
+interface ActivityLevelScaleDao {
+
+    // TODO
+
+}
+
+
